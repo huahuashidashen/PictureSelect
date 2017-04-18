e7baa1f03c74e14650c8583fe124812ddf9729e3
gaolinhua <1518810211@qq.com>
2017-04-18 23:14:11 +0800

    Revert "显示手机图库的代码"
    This reverts commit 93a05eb4e1ecb77885f37f1d26477f593739844e.

--- app/src/main/java/com/huahua/gallery/MainActivity.java
+++ app/src/main/java/com/huahua/gallery/MainActivity.java
@@ -3,18 +3,11 @@ package com.huahua.gallery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_CODE_GALLERY = 1;
-    private GalleryFinal.OnHandlerResultCallback mOnHandlerResultCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //打开相册
-        GalleryFinal.openGallerySingle(this,REQUEST_CODE_GALLERY, mOnHandlerResultCallback);
     }
-
 }
--- app/src/main/java/com/huahua/gallery/ShowPhotoActivity.java
+++
@@ -1,9 +0,0 @@
-package com.huahua.gallery;
-
-/**
- * Created by administror on 2017/4/18 0018.
- */
-
-public class ShowPhotoActivity {
-
-}
